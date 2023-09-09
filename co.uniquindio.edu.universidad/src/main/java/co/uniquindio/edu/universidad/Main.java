43f3e7a157df79f64b47fc92da8aabd8864b3cf6
Estefmd <142418723+Estefmd@users.noreply.github.com>
2023-09-09 15:11:50 -0500

    Se inician datos

--- co.uniquindio.edu.universidad/src/main/java/co/uniquindio/edu/universidad/Main.java
+++ co.uniquindio.edu.universidad/src/main/java/co/uniquindio/edu/universidad/Main.java
@@ -1,66 +1,43 @@
 package co.uniquindio.edu.universidad;
+
 import co.uniquindio.edu.universidad.model.Estudiante;
 import co.uniquindio.edu.universidad.model.Docente;
 import co.uniquindio.edu.universidad.model.Curso;
+
 public class Main {
-
     public static void main(String[] args) {
-
-        Estudiante estudiante1 = crearEstudiante("Juan", 17, "juan@mail.com", 3, 3, 3.5, 4.5);
-        Estudiante estudiante2 = crearEstudiante("Maria", 15, "pepita@mail.com", 1, 2, 2.5, 4.5);
-        Estudiante estudiante3 = crearEstudiante("Pepe", 20, "pepe@mail.com", 4,4,2.5,4);
-        Docente docenteOscar = new Docente("Oscar Salazar", 36, "oscarS@mail.com");
-        Curso curso  = new Curso("Programacion 1", 2, "02-N", 3, "Nocturna");
-
-        curso.setEstudiante1(estudiante1);
-        curso.setEstudiante2(estudiante2);
-        curso.setEstudiante3(estudiante3);
-
-        curso.setDocente(docenteOscar);
-
-        docenteOscar.mostrarDefinitivaEstudiantes(estudiante1, estudiante2, estudiante3);
-
-        docenteOscar.mostrarPromedioCurso(estudiante1, estudiante2, estudiante3);
-
-        System.out.println("El promedio de edad de los tres estudiantes es: " + docenteOscar.calcularPromedioEdad(estudiante1, estudiante2, estudiante3));
-
-        System.out.println("El promedio de la nota 1 de los tres estudiantes es: " + docenteOscar.calcularPromedioNota1(estudiante1, estudiante2, estudiante3));
-
-        System.out.println("La nota mayor del curso es: " + docenteOscar.calcularNotaMayorDelCurso(estudiante1, estudiante2, estudiante3));
-
-        System.out.println("La nota menor del curso es: " + docenteOscar.calcularNotaMenorDelCurso(estudiante1, estudiante2, estudiante3));
-
-        if (docenteOscar.verificarAprobacionCurso(estudiante1)){
-            System.out.println("El Estudiante " + estudiante1.getNombre() + " aprobo el curso");
-        }
-        if (docenteOscar.verificarAprobacionCurso(estudiante2)){
-            System.out.println("El Estudiante " + estudiante2.getNombre() + " aprobo el curso");
-        }
-        if (docenteOscar.verificarAprobacionCurso(estudiante3)){
-            System.out.println("El Estudiante " + estudiante3.getNombre() + " aprobo el curso");
-        }
-
-        System.out.println("El porcentaje de estudiantes que aprobaron el curso es: "+ docenteOscar.calcularAprobaronCurso(estudiante1,estudiante2,estudiante3)+"%");
-
-        System.out.println("El porcentaje de estudiantes que reprobaron el curso es: "+ docenteOscar.calcularReprobaronCurso(estudiante1,estudiante2,estudiante3)+"%");
-
-        if (docenteOscar.obtenerEstudianteNotaMayor4(estudiante1)){
-            System.out.print("Todas las notas de: " + estudiante1.getNombre() + "son mayores a 4.0");
-        }
-        if (docenteOscar.obtenerEstudianteNotaMayor4(estudiante2)){
-            System.out.print("Todas las notas de: " + estudiante2.getNombre() + "son mayores a 4.0");
-        }
-        if (docenteOscar.obtenerEstudianteNotaMayor4(estudiante3)){
-            System.out.print("Todas las notas de: " + estudiante3.getNombre() + "son mayores a 4.0");
-        }
-        System.out.println("La nota mayor de  " +estudiante1.getNombre() + " es: " + docenteOscar.obtenerNotaMaximaEstudiante(estudiante1));
-        System.out.println("La nota mayor de  " +estudiante2.getNombre() + " es: " + docenteOscar.obtenerNotaMaximaEstudiante(estudiante2));
-        System.out.println("La nota mayor de  " +estudiante3.getNombre() + " es: " + docenteOscar.obtenerNotaMaximaEstudiante(estudiante3));
-
+        Curso curso = inicializaDatosPrueba();
+        
     }
+        private static Curso inicializaDatosPrueba() {
+            Estudiante estudiante1 = crearEstudiante("Juan",17, "juan@mail.com", 3, 3, 3.5, 4.5);
+            Estudiante estudiante2 = crearEstudiante("Maria", 15, "pepita@mail.com", 1, 2, 2.5, 4.5);
+            Estudiante estudiante3 = crearEstudiante("Pepe", 20, "pepe@mail.com", 4, 4, 2.5, 4);
+            Docente docente = new Docente("Oscar Salazar", 36, "oscarS@mail.com");
+            Curso curso = new Curso("Programacion 1", 2, "02-N", 3, "Nocturna");
+            curso.setEstudiante1(estudiante1);
+            curso.setEstudiante2(estudiante2);
+            curso.setEstudiante3(estudiante3);
+            curso.setDocente(docente);
 
-    private static Estudiante crearEstudiante(String nombre, int edad,String correo,int semestre,double nota1, double nota2, double nota3) {
+            return curso;
+        }
+
+        private static Estudiante crearEstudiante(String nombre, int edad, String correo, int semestre, double nota1, double nota2, double nota3){
+        Estudiante estudiante = new Estudiante();
+        estudiante.setNombre(nombre);
+        estudiante.setEdad(edad);
+        estudiante.setCorreo(correo);
+        estudiante.setSemestre(semestre);
+        estudiante.setNota1(nota1);
+        estudiante.setNota2(nota2);
+        estudiante.setNota3(nota3);
+
+        return estudiante;
+        }
+    private static Estudiante crearEstudiantePorConstructor(String nombre, int edad,String correo,int semestre,double nota1, double nota2, double nota3) {
         Estudiante estudiante = new Estudiante(nombre, edad, correo, semestre, nota1, nota2, nota3);
+
         return estudiante;
     }
 }
--- co.uniquindio.edu.universidad/src/main/java/co/uniquindio/edu/universidad/model/Curso.java
+++ co.uniquindio.edu.universidad/src/main/java/co/uniquindio/edu/universidad/model/Curso.java
@@ -92,4 +92,5 @@ public class Curso {
     public void setDocente(Docente docente) {
         this.docente = docente;
     }
+
 }
